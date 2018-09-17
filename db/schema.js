1d6ac006262fb8b825bebf893fcafc4b653b10df
elfein <elfein99@gmail.com>
2018-09-17 11:21:23 -0400

    schema file completed

---
+++ db/schema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Scehma
+
+const GigSchema = new Schema({
+    venue: String,
+    city: String,
+    date: String,
+    price: Number
+})
+
+const BandSchema = new Schema({
+    name: String,
+    genre: String,
+    gigs: [GigSchema]
+})
+
+const UserSchema = new Schema({
+    username: String,
+    city: String,
+    bands: [BandSchema]
+})
+
+const GigModel = mongoose.model('Gig', GigSchema)
+const BandModel = mongoose.model('Band', BandSchema)
+const UserModel = mongoose.model('User', UserSchema)
+
+module.exports = { Gig: GigModel, Band: BandModel, User: UserModel }
